3907cf31cfb1a0aa88eb23cd54fc973c07f6a28e
Sandeep <rayanuthalas@gmail.com>
2020-04-27 15:51:40 +0530

    Final Changes

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""
-This code scraps mohfw.gov.in for COVID-19 Data
 
-"""
 import requests
 import threading
 from bs4 import BeautifulSoup
@@ -15,24 +12,10 @@ import time
 from time import sleep
 from datetime import datetime
 from algo import scrapper
+
+
+
 app = Flask(__name__)
-
-# Removed Foreign National Column
-headers = {
-    0: "id",
-    1: "place",
-    2: "confirmed",
-    3: "cured",
-    4: "deaths"
-}
-# Added Date Time matching REGEX to give better input to Fuzze Dateutil Parser
-date_time_pattern = r"\d{2}.\d{2}.2020(.*)"
-last_extracted_content = "welcome"
-last_extracted_time = "2020-03-28 17:45:00"
-
-
-
-
 
 
 @app.route('/', methods=['GET'])
@@ -61,6 +44,4 @@ def get_data():
 
 
 if __name__ == "__main__":
-    # #thread.start_new_thread(data_extract, ())
-    # x.start()
     app.run(debug=True)
